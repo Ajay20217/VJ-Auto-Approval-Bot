4f0aaf9fd122444c5861f006e752970b09d994be
Ajay20217 <106938620+Ajay20217@users.noreply.github.com>
2024-05-20 22:30:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,13 +6,13 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "11893421"))
-    API_HASH = getenv("API_HASH", "f571101b32748bc6f8e2178d03c12dd0")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6916331007:AAGhWSWkrA-ht47oFtJF16DVYri4l1epUM0")
-    FSUB = getenv("FSUB", "omglinksofficial")
-    CHID = int(getenv("CHID", "-1001635195087"))
-    SUDO = list(map(int, getenv("SUDO", "6036497605").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://umpiremovie:sYc77AAESIugtrQV@cluster0.lpdoczo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    API_ID = int(getenv("API_ID", ""))
+    API_HASH = getenv("API_HASH", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    FSUB = getenv("FSUB", "")
+    CHID = int(getenv("CHID", ""))
+    SUDO = list(map(int, getenv("SUDO", "").split()))
+    MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
 
